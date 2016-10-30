372b0af87e27ad1daaee4714971f63a60c7ea193
drklrd <srvbhattarai@gmail.com>
2016-10-30 15:20:25 +0545

    fallback to previous code

--- controllers/real-time-engine/index.js
+++ controllers/real-time-engine/index.js
@@ -9,9 +9,6 @@ module.exports = (router,server)=>{
 
 	var io = socketIO(server);
 
-	io.set('transports', ['xhr-polling']);
-	io.set('polling duration', 10);
-
 	io.on('connection',(socket)=>{
 		
 
